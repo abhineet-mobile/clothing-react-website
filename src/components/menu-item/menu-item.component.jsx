347985c07c0adc4ad11ff214308375366b20508c
abhineet-mobile <avi.program@gmail.com>
2020-05-27 21:40:57 +0530

    url routing basic

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react'; 
-
+import {withRouter} from 'react-router-dom'
 import './menu-item.styles.scss';
 
 
-const MenuItems = ({title , imageUrl, size}) => (
-<div className= {`${size} menu-item`}>
+const MenuItems = ({title , imageUrl, size , history, match, linkUrl }) => (
+<div className= {`${size} menu-item`} onClick={()=> history.push(`${match.url}${linkUrl}`)}>
     <div className='background-image'
     style= {{
         backgroundImage:`url(${imageUrl})`
@@ -17,4 +17,4 @@ const MenuItems = ({title , imageUrl, size}) => (
 </div>
 
 ); 
-export default MenuItems; 
+export default withRouter (MenuItems); 

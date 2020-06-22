92adabcc9fcb13e9209006138901c6b8c27a1514
abhineet-mobile <avi.program@gmail.com>
2020-06-22 21:44:39 +0530

    add item and remove item in the checkout page

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -7,4 +7,13 @@ export const toggelCartHidden = ()=>({
 export const addItem = item =>({
     type: cartActionTypes.ADD_CART_ITEM, 
     payload : item
-})
+}); 
+export const removeItem =item =>({
+    type: cartActionTypes.REMOVE_ITEM, 
+    payload: item
+});
+
+export const clearItemFromCart = item =>({
+    type:cartActionTypes.CLEAR_ITEM_FROM_CART, 
+    payload: item
+});
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import {cartActionTypes} from './cart.type'; 
-import {addItemToCart} from './cart.utils';
+import {addItemToCart , removeItemFromCart} from './cart.utils';
 const INITIAL_STATE ={
     hidden: true, 
     cartItems: []
@@ -11,11 +11,19 @@ const cartReducer =(state=INITIAL_STATE, action )=>{
         case cartActionTypes.TOGGEL_CART_HIDDEN: return{
                 ...state,
                 hidden: !state.hidden
-        }
+        };
         case cartActionTypes.ADD_CART_ITEM: return{
             ...state, 
             cartItems: addItemToCart(state.cartItems, action.payload)
         };
+        case cartActionTypes.REMOVE_ITEM: return{
+            ...state, 
+            cartItems: removeItemFromCart(state.cartItems, action.payload)
+        };
+        case cartActionTypes.CLEAR_ITEM_FROM_CART:return{
+            ...state, 
+            cartItems: state.cartItems.filter( cartItem => cartItem.id !== action.payload.id)
+        }
         default:
             return state;
     }
